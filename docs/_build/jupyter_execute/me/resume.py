d1ccc413c76c653d26f598acef06c549af9ef720
Jirayus Jiarpakdee <jirayus.jiar@gmail.com>
2021-04-27 16:50:43 +1000

    Published Version 3

--- docs/_build/jupyter_execute/me/resume.py
+++ docs/_build/jupyter_execute/me/resume.py
@@ -6,24 +6,10 @@
 # In[1]:
 
 
-class PDF(object):
-  def __init__(self, pdf, size=(200,200)):
-    self.pdf = pdf
-    self.size = size
-
-  def _repr_html_(self):
-    return '<iframe src={0} width={1[0]} height={1[1]}></iframe>'.format(self.pdf, self.size)
-
-  def _repr_latex_(self):
-    return r'\includegraphics[width=1.0\textwidth]{{{0}}}'.format(self.pdf)
-
-
 from IPython.display import IFrame, display
 
 filepath = "https://raw.githubusercontent.com/jirayusjiar/jirayusjiar.github.io/master/docs/me/jirayus_resume.pdf"
-IFrame(filepath, width=595, height=841)
-
-#PDF('jirayus_resume.pdf',size=(595,841))
+IFrame(filepath, width=50, height=50)
 
 
 # *This page has been updated on 21st April, 2021.* 
